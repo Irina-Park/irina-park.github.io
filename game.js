ebd00d2a09a82f572a28e64d01bbf3d4a706c37e
Irina-Park <63580209+Irina-Park@users.noreply.github.com>
2020-06-04 10:11:03 +0200

    Create game.js

---
+++ game.js
@@ -0,0 +1,10 @@
+var myNoun1 = prompt("Enter a noun");
+var myNoun2 = prompt("Enter another noun");
+var myAdjective = prompt("Enter an adjective");
+var myVerb = prompt("Enter a verb");
+var myAdverb = prompt("Enter an adverb");
+
+// Only change code below this line
+var wordBlanks = "JD was a smart " + myAdjective + " " + myNoun1 + " and one day he sees a " + myNoun2 + " and he " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
+alert(wordBlanks);
